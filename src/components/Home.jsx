80c6802e03922e9288c8d6f20cbe4c51539daba0
Selormey Hubert <selormeyh@gmail.com>
2023-12-24 10:48:30 +0000

    home changes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,32 +1,56 @@
- // src/pages/Home.jsx
-import React from 'react';
+import React, { useRef } from 'react';
 import CaptureButton from './layout/CaptureButton';
 
 const Home = () => {
+  const videoRef = useRef(null);
+
   const handleCapture = async () => {
     try {
       const stream = await navigator.mediaDevices.getUserMedia({ video: true });
 
-      // Access the camera stream, you can do further processing with the stream if needed
+      // Access the camera stream
+      videoRef.current.srcObject = stream;
 
-      // For example, you might want to display the camera stream in a video element
-      const videoElement = document.createElement('video');
-      videoElement.srcObject = stream;
-      videoElement.autoplay = true;
-      document.body.appendChild(videoElement);
+      // Display the camera stream in a video element
+      videoRef.current.play();
     } catch (error) {
       console.error('Error accessing camera:', error);
     }
   };
 
+  const takeSnapshot = () => {
+    const video = videoRef.current;
+
+    // Ensure the video is playing
+    if (video.paused) {
+      video.play();
+    }
+
+    // Create a canvas element to capture the frame
+    const canvas = document.createElement('canvas');
+    canvas.width = video.videoWidth;
+    canvas.height = video.videoHeight;
+    const context = canvas.getContext('2d');
+    context.drawImage(video, 0, 0, canvas.width, canvas.height);
+
+    // Convert the captured frame to a data URL (base64-encoded image)
+    const imageDataUrl = canvas.toDataURL('image/png');
+
+    // Log or use the imageDataUrl as needed
+    console.log('Captured Image:', imageDataUrl);
+
+    // Stop the camera stream after capturing the image
+    const streamTracks = video.srcObject.getTracks();
+    streamTracks.forEach((track) => track.stop());
+  };
+
   return (
     <div className="text-center">
-       
       <div className="bg-blue-500 rounded-md p-4 mb-4 h-48 w-64">
-        {/* Your content here */}
-        
+        {/* Display the captured image here */}
+        <video ref={videoRef} className="w-full h-full" style={{ display: 'none' }}></video>
       </div>
-      <CaptureButton onCapture={handleCapture} />
+      <CaptureButton onCapture={takeSnapshot} />
     </div>
   );
 };
